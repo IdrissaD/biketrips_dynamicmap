3b56df448b129430cda22df582eb3689a38c3d35
IdrissaD <73114156+IdrissaD@users.noreply.github.com>
2020-11-28 23:25:08 +0100

    Styling and short routes
    Simplify styling and fix the "routes entirely contained in a department" issue

--- choromap.js
+++ choromap.js
@@ -9,103 +9,10 @@ var choromap = L.map('choroid').setView([45.757523270000576, 4.831581115722656],
 
 // Define color scale according to length
 function style_dpts(feature) {
-            if (length == 0.000000) {
-                return {
+            return {
                 opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
-                fillOpacity: 0,
-                fillColor: 'rgba(255,255,255,1.0)',
-                interactive: true,
-            }
-            }
-            if (length >= 0.000000 && length <= 50.000000 ) {
-                return {
-                opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
                 fillOpacity: 0.65,
-                fillColor: 'rgba(247,252,245,1.0)',
-                interactive: true,
-            }
-            }
-            if (length >= 50.000000 && length <= 100.000000 ) {
-                return {
-                opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
-                fillOpacity: 0.65,
-                fillColor: 'rgba(213,239,207,1.0)',
-                interactive: true,
-            }
-            }
-            if (length >= 100.000000 && length <= 150.000000 ) {
-                return {
-                opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
-                fillOpacity: 0.65,
-                fillColor: 'rgba(158,216,152,1.0)',
-                interactive: true,
-            }
-            }
-            if (length >= 150.000000 && length <= 200.000000 ) {
-                return {
-                opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
-                fillOpacity: 0.65,
-                fillColor: 'rgba(84,181,103,1.0)',
-                interactive: true,
-            }
-            }
-            if (length >= 200.000000 && length <= 250.000000 ) {
-                return {
-                opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
-                fillOpacity: 0.65,
-                fillColor: 'rgba(29,134,65,1.0)',
-                interactive: true,
-            }
-            }
-            if (length >= 250.000000) {
-                return {
-                opacity: 0,
-                color: 'rgba(35,35,35,1.0)',
-                dashArray: '',
-                lineCap: 'butt',
-                lineJoin: 'miter',
-                weight: 1.0, 
-                fill: true,
-                fillOpacity: 0.65,
-                fillColor: 'rgba(0,68,27,1.0)',
-                interactive: true,
-            }
+                fillColor: getColor(length)
             }
 };
 
@@ -113,11 +20,16 @@ function style_dpts(feature) {
 function onEachFeature(feature, layer)
         {layer.bindPopup('<p><b>' + feature.properties.nom + '</b></p><p>'+ parseFloat(length).toFixed(0) + ' km</p>')};
 
-var length = 0
 
-
+// Prepare layer of dpts, ready to be added data
+var dpts_layer = L.geoJSON(null, {
+    onEachFeature: onEachFeature,
+    style: style_dpts
+});
 
 // Loop over dpts and routes
+var length = 0
+
 for (dpt of dpts.features) {
 	var length = 0
 	for (route of routes.features) {
@@ -126,6 +38,11 @@ for (dpt of dpts.features) {
 		// split the line against the geom of the dpt
 		let split = turf.lineSplit(turf.lineString(part), dpt);
 		//console.log(split.features);
+		// try to check if the route is contained in the dpt, in which case length = full length of the route
+		if (turf.booleanPointInPolygon(turf.point(part[0]), dpt) && split.features.length == 0) {
+				length = length + turf.length(route);
+				//console.log(length);
+			}
 		let oddPair;
 		// check if the first point of the spliced line is in the dpt
 
@@ -137,16 +54,13 @@ for (dpt of dpts.features) {
 		// only get the length of 1 out of 2 segments of each route (a segment contained in a dpt has to be surrounded by two segments not contained)
 		split.features.forEach((splitedPart, i) => {
 			//console.log(i+oddPair);
-			if((i + oddPair)%2 === 0) {
+			if(turf.booleanPointInPolygon(turf.point(splitedPart.geometry.coordinates[splitedPart.geometry.coordinates.length -2]), dpt)) {
 				//L.geoJSON(splitedPart.geometry, {style: function(feature){return{color: "green"}}}).addTo(choromap);
 				length = length + turf.length(splitedPart);
 				//console.log(length);
 		}});
 	})}
-	L.geoJson(dpt, {
-		onEachFeature: onEachFeature,
-        style: style_dpts
-    }).addTo(choromap)
+	dpts_layer.addData(dpt)
 };
        
 // Define the colors for the legend
@@ -157,7 +71,7 @@ function getColor(d) {
            d > 100   ? 'rgba(158,216,152,1.0)' :
            d > 50   ? 'rgba(213,239,207,1.0)' :
            d > 0   ? 'rgba(247,252,245,1.0)' :
-           'rgba(255,255,255,1.0)';
+           'rgba(255,255,255,0.0)';
 }
 
 // Crate a legend control
@@ -181,5 +95,6 @@ legend.onAdd = function (map) {
     return div;
 };
 
+dpts_layer.addTo(choromap);
 osm_layer.addTo(choromap);
 legend.addTo(choromap);
